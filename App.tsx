71f765b7be8d3e288ccc00bd5b42436a14aeab5a
lapprenti18 <pokspid@gmail.com>
2020-11-15 21:20:22 +0100

    add first screen

---
+++ App.tsx
@@ -0,0 +1,28 @@
+import * as React from "react";
+import { createStackNavigator } from "@react-navigation/stack";
+
+import { Onboarding } from "./src/Authentication";
+import { LoadAssets } from "./src/components";
+
+const fonts = {
+  "SFProText-Bold": require("./assets/fonts/SF-Pro-Text-Bold.otf"),
+  "SFProText-Semibold": require("./assets/fonts/SF-Pro-Text-Semibold.otf"),
+  "SFProText-Regular": require("./assets/fonts/SF-Pro-Text-Regular.otf"),
+};
+
+const AuthenticationStack = createStackNavigator();
+const AuthenticationNavigator = () => {
+  return (
+    <AuthenticationStack.Navigator headerMode="none">
+      <AuthenticationStack.Screen name="OnBoarding" component={Onboarding} />
+    </AuthenticationStack.Navigator>
+  );
+};
+
+export default function App() {
+  return (
+    <LoadAssets {...{ fonts }}>
+      <AuthenticationNavigator />
+    </LoadAssets>
+  );
+}
---
+++ src/Authentication/Onboarding/Onboarding.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+import { View } from "react-native";
+
+interface ComponentNameProps {}
+
+const ComponentName = () => {
+    return <view style= {{ flex: 1, background: "cyan"}}/>;
+};
+
+export default ComponentName;
